51736500f955b74e31dfee0b290772c9572ab61e
bbm <brian.maranville@nist.gov>
2024-10-15 17:02:31 -0400

    set version to 0.2.0alpha-0 prior to publishing to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='molgroups',
-    version='0.2.0',
+    version='0.2.0a0',
     packages=['molgroups'],
     url='https://github.com/criosx/molgroups',
     license='MIT License',
