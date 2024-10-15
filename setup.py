f0b7ab818b4c31dc5467d8315cf21e2075d6082b
bbm <brian.maranville@nist.gov>
2024-10-15 17:10:29 -0400

    add long_description, needed for pypi publish

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='molgroups',
     version='0.2.0a0',
@@ -9,6 +14,8 @@ setup(
     author='Frank Heinrich, David Hoogerheide, Alyssa Thomas',
     author_email='mail@frank-heinrich.net',
     description='Molecular Modeling for Scattering Data Analysis',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         "bumps", "refl1d", "periodictable", "scipy", "numpy", "matplotlib", "pandas", "scikit-learn",
         "sasmodels", "sasdata", "dill",
