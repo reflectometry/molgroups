eeb784c35d2b7291e078994e6019c05d925e67f6
hoogerheide <david.hoogerheide@nist.gov>
2025-01-27 12:57:56 -0500

    more sophisticated model copying for make_samples

--- molgroups/refl1d_interface/experiment.py
+++ molgroups/refl1d_interface/experiment.py
@@ -5,7 +5,8 @@ from dataclasses import dataclass, fields
 import copy
 import functools
 
-from refl1d.names import Experiment, Stack, SLD, MixedExperiment
+from refl1d.names import Experiment, Stack, SLD, MixedExperiment, Parameter
+from bumps.parameter import Expression
 
 from .groups import MolgroupsInterface, ReferencePoint
 from .layers import MolgroupsStack, MolgroupsLayer
@@ -73,15 +74,92 @@ def _copy_group(gp: MolgroupsInterface | None) -> MolgroupsInterface | None:
         new_gp._generate_id()
 
         # reset ReferencePoints
-        #for f in fields(new_gp):
-        #    if f.type == ReferencePoint:
-        #        setattr(new_gp, f.name, f.default_factory())
+        for f in fields(new_gp):
+            if f.type == ReferencePoint:
+                setattr(new_gp, f.name, f.default_factory())
 
         # run post init
         new_gp.__post_init__()
 
         return new_gp
+    
 
+def _copy_template(layer_template: MolgroupsLayer, contrast: SLD) -> MolgroupsLayer:
+    """Copies a template and associates a contrast with it. This is a
+        complex procedure because there can
+        be logic linking the ReferencePoint objects in each group to
+        parameters via Expression objects. These need to be reconstructed
+        in the copy.
+    
+    Args:
+        layer_template (MolgroupsLayer): template layer
+
+    Returns:
+        MolgroupsLayer: copied template
+    """
+
+    # TODO: this does not work for deep groups like BLMProteinComplex
+
+    # find normarea group if it exists
+    normarea_id = layer_template.normarea_group.id if layer_template.normarea_group is not None else None
+
+    # find which group is the normarea group
+    if normarea_id is not None:
+        all_groups = [layer_template.base_group] + layer_template.add_groups + layer_template.overlay_groups
+        normarea_index = [gp.id for gp in all_groups].index(normarea_id)
+
+    # create a dictionary of which attribute corresponds to which reference point
+    all_groups = [layer_template.base_group] + layer_template.add_groups + layer_template.overlay_groups
+    new_base_group = _copy_group(layer_template.base_group)
+    new_add_groups = [_copy_group(gp) for gp in layer_template.add_groups]
+    new_overlay_groups = [_copy_group(gp) for gp in layer_template.overlay_groups]
+    new_groups = [new_base_group] + new_add_groups + new_overlay_groups
+
+    references = {}
+
+    # recursive function to build up references list
+    def _build_references(gps: List[MolgroupsInterface]):
+        for i, gp in enumerate(gps):
+            for f in fields(gp):
+                attr = getattr(gp, f.name)
+                if f.type == ReferencePoint:
+                    rp: ReferencePoint = getattr(gp, f.name)
+                    references[rp.id] = dict(index=i,
+                                            group_id=gp.id,
+                                            field_name=f.name)
+                    
+                elif isinstance(attr, MolgroupsInterface):
+                    _build_references([attr])
+
+    # create recursive function to replace all Expressions with copies with unique IDs and the appropriate references
+    def _replace_references(exp: Expression):
+        new_exp = copy.copy(exp)
+        new_exp_args = list(new_exp.args)
+        for i, arg in enumerate(new_exp_args):
+            if isinstance(arg, ReferencePoint):
+                ref = references[arg.id]
+                new_exp_args[i] = getattr(new_groups[ref['index']], ref['field_name'])
+            elif isinstance(arg, Expression):
+                new_exp_args[i] = _replace_references(arg)
+
+        new_exp.args = new_exp_args
+
+        return new_exp
+
+    for gp in new_groups:
+        for f in fields(gp):
+            attr = getattr(gp, f.name)
+            if isinstance(attr, Expression):
+                setattr(gp, f.name, _replace_references(attr))
+                
+    return MolgroupsLayer(base_group=new_base_group,
+                              normarea_group=new_groups[normarea_index] if normarea_id is not None else None,
+                            add_groups=new_add_groups,
+                            overlay_groups=new_overlay_groups,
+                            contrast=contrast,
+                            thickness=layer_template.thickness,
+                            name=contrast.name + ' ' + layer_template.name)
+    
 def make_samples(layer_template: MolgroupsLayer, substrate: Stack, contrasts: List[SLD]) -> List[MolgroupsStack]:
 
     """Create samples from combining a substrate stack with a molgroups layer
@@ -96,42 +174,8 @@ def make_samples(layer_template: MolgroupsLayer, substrate: Stack, contrasts: Li
     """
     samples = []
 
-    normarea_id = layer_template.normarea_group.id if layer_template.normarea_group is not None else None
-
-    # find which group is the normarea group
-    if normarea_id is not None:
-        all_groups = [layer_template.base_group] + layer_template.add_groups + layer_template.overlay_groups
-        normarea_index = [gp.id for gp in all_groups].index(normarea_id)
-
-    for k, contrast in enumerate(contrasts):
-        if k == 0:
-            mollayer = MolgroupsLayer(base_group=layer_template.base_group,
-                                    add_groups=layer_template.add_groups,
-                                    overlay_groups=layer_template.overlay_groups,
-                                    contrast=contrast,
-                                    thickness=layer_template.thickness,
-                                    name=contrast.name + ' ' + layer_template.name)
-
-            all_groups0 = [mollayer.base_group] + mollayer.add_groups + mollayer.overlay_groups
-
-        else:
-            mollayer = MolgroupsLayer(base_group=_copy_group(layer_template.base_group),
-                                    add_groups=[_copy_group(gp) for gp in layer_template.add_groups],
-                                    overlay_groups=[_copy_group(gp) for gp in layer_template.overlay_groups],
-                                    contrast=contrast,
-                                    thickness=layer_template.thickness,
-                                    name=contrast.name + ' ' + layer_template.name)
-            
-            for gp0, gp in zip(all_groups0, [mollayer.base_group] + mollayer.add_groups + mollayer.overlay_groups):
-                for f in fields(gp0):
-                    if f.type == ReferencePoint:
-                        setattr(gp, f.name, getattr(gp0, f.name))
-
-        # apply normarea group if applicable
-        if normarea_id is not None:
-            all_groups = [mollayer.base_group] + mollayer.add_groups + mollayer.overlay_groups
-            mollayer.normarea_group = all_groups[normarea_index]
-        
+    for contrast in contrasts:
+        mollayer = _copy_template(layer_template, contrast)
         samples.append(MolgroupsStack(substrate=substrate,
                                       molgroups_layer=mollayer,
                                       name=mollayer.name))
